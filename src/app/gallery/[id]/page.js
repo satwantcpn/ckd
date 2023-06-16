657fd168eaa44e7a83a69865a04cb5e60445ab05
satwantcpn <satwant@cyberpuzzlenet.com>
2023-06-16 18:25:19 +0530

    gallery update

--- src/app/gallery/[id]/page.js
+++ src/app/gallery/[id]/page.js
@@ -9,42 +9,82 @@ export default function SingleGallery() {
                     <div className="row">
                         <div className="col-md-3 mb-3">
                             <div className="singleGalleryImg">
-                                <img src="/gallery1.jpg" alt="Gallery Image" />
+                                <img src="/gk1.png" alt="Gallery Image" />
                             </div>
                         </div>
                         <div className="col-md-3 mb-3">
                             <div className="singleGalleryImg">
-                                <img src="/gallery1.jpg" alt="Gallery Image" />
+                                <img src="/gk2.png" alt="Gallery Image" />
                             </div>
                         </div>
                         <div className="col-md-3 mb-3">
                             <div className="singleGalleryImg">
-                                <img src="/gallery1.jpg" alt="Gallery Image" />
+                                <img src="/gk3.png" alt="Gallery Image" />
                             </div>
                         </div>
                         <div className="col-md-3 mb-3">
                             <div className="singleGalleryImg">
-                                <img src="/gallery1.jpg" alt="Gallery Image" />
+                                <img src="/gk4.png" alt="Gallery Image" />
                             </div>
                         </div>
                         <div className="col-md-3 mb-3">
                             <div className="singleGalleryImg">
-                                <img src="/gallery1.jpg" alt="Gallery Image" />
+                                <img src="/gk5.png" alt="Gallery Image" />
                             </div>
                         </div>
                         <div className="col-md-3 mb-3">
                             <div className="singleGalleryImg">
-                                <img src="/gallery1.jpg" alt="Gallery Image" />
+                                <img src="/gk6.png" alt="Gallery Image" />
                             </div>
                         </div>
                         <div className="col-md-3 mb-3">
                             <div className="singleGalleryImg">
-                                <img src="/gallery1.jpg" alt="Gallery Image" />
+                                <img src="/gk7.png" alt="Gallery Image" />
                             </div>
                         </div>
                         <div className="col-md-3 mb-3">
                             <div className="singleGalleryImg">
-                                <img src="/gallery1.jpg" alt="Gallery Image" />
+                                <img src="/gk8.png" alt="Gallery Image" />
+                            </div>
+                        </div>
+                        <div className="col-md-3 mb-3">
+                            <div className="singleGalleryImg">
+                                <img src="/gk9.png" alt="Gallery Image" />
+                            </div>
+                        </div>
+                        <div className="col-md-3 mb-3">
+                            <div className="singleGalleryImg">
+                                <img src="/gk10.png" alt="Gallery Image" />
+                            </div>
+                        </div>
+                        <div className="col-md-3 mb-3">
+                            <div className="singleGalleryImg">
+                                <img src="/gk11.png" alt="Gallery Image" />
+                            </div>
+                        </div>
+                        <div className="col-md-3 mb-3">
+                            <div className="singleGalleryImg">
+                                <img src="/gk12.png" alt="Gallery Image" />
+                            </div>
+                        </div>
+                        <div className="col-md-3 mb-3">
+                            <div className="singleGalleryImg">
+                                <img src="/gk13.png" alt="Gallery Image" />
+                            </div>
+                        </div>
+                        <div className="col-md-3 mb-3">
+                            <div className="singleGalleryImg">
+                                <img src="/gk14.png" alt="Gallery Image" />
+                            </div>
+                        </div>
+                        <div className="col-md-3 mb-3">
+                            <div className="singleGalleryImg">
+                                <img src="/gk15.png" alt="Gallery Image" />
+                            </div>
+                        </div>
+                        <div className="col-md-3 mb-3">
+                            <div className="singleGalleryImg">
+                                <img src="/gk16.png" alt="Gallery Image" />
                             </div>
                         </div>
                     </div>
